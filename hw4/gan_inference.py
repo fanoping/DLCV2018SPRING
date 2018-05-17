a1f2e8a79827c0cb40cae54b43f4462aedc6f20c
fanoping <fanoping@gmail.com>
2018-05-18 03:12:49 +0800

    all finished, modify seed, script fix reproduce

--- hw4/gan_inference.py
+++ hw4/gan_inference.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
     parser.add_argument('--output-file', default='saved/gan',
                         help='output data directory')
     parser.add_argument('--checkpoint',
-                        default='checkpoints/gan/epoch231_checkpoint.pth.tar',
+                        default='checkpoints/gan/epoch230_checkpoint.pth.tar',
                         help='load checkpoint')
     parser.add_argument('--no-cuda', action='store_true',
                         help='use CPU in case there\'s no GPU support')
--- hw4/hw4.sh
+++ hw4/hw4.sh
@@ -1,7 +1,7 @@
 wget -O vae_best_checkpoint.pth.tar https://www.dropbox.com/s/aqwfaq2ofzgkuro/vae_best_checkpoint.pth.tar?dl=1
 python3 vae_inference.py --input-file $1 --output-file $2 --checkpoint vae_best_checkpoint.pth.tar
-wget -O gan_epoch231_checkpoint.pth.tar https://www.dropbox.com/s/xpx6n6jhl266dzm/gan_epoch231_checkpoint.pth.tar?dl=1
-python3 gan_inference.py --output-file $2 --checkpoint gan_inference.py --output-file
+wget -O gan_epoch230_checkpoint.pth.tar https://www.dropbox.com/s/t7jnvbj0oawnhdi/gan_epoch230_checkpoint.pth.tar?dl=1
+python3 gan_inference.py --output-file $2 --checkpoint gan_epoch230_checkpoint.pth.tar
 wget -O acgan_epoch300_checkpoint.pth.tar https://www.dropbox.com/s/1017nvp3rnsu5x0/acgan_epoch300_checkpoint.pth.tar?dl=1
 python3 acgan_inference.py --output-file $2 --checkpoint acgan_epoch300_checkpoint.pth.tar
 wget -O infogan_epoch86_checkpoint.pth.tar https://www.dropbox.com/s/3whz6sk5ahnedf8/infogan_epoch86_checkpoint.pth.tar?dl=1
