07fa4f08579d5df758ee9b67d20e40510332bfd3
fanoping <fanoping@gmail.com>
2018-05-26 00:21:40 +0800

    cnn modify finish

--- hw5/utils/dataset.py
+++ hw5/utils/dataset.py
@@ -6,28 +6,36 @@ import pickle
 
 
 class TrimmedVideo(Dataset):
-    def __init__(self, mode='train'):
+    def __init__(self, mode='train', pool=True):
         super(TrimmedVideo, self).__init__()
         self.with_cuda = True
         self.mode = mode
+        self.pool = pool
         self.__load_data()
 
     def __load_data(self):
         print("Loading {} data".format(self.mode))
         if self.mode == 'train':
-            with open('train_video.pkl', 'rb') as f:
-                self.train_video = pickle.load(f)
+            self.train_video = torch.load('train_video.tar')
 
+            if self.pool:
+                self.train_video = [torch.mean(frames, dim=0) for _, frames in self.train_video.items()]
+                self.train_video = torch.stack(self.train_video)
+            print(self.train_video.size())
             self.train_label = getVideoList('HW5_data/TrimmedVideos/label/gt_train.csv')['Action_labels']
             self.train_label = np.array(self.train_label).astype(np.uint8)
-            self.train_label = torch.from_numpy(self.train_label).type(torch.long)
+            self.train_label = torch.LongTensor(self.train_label)
+
         else:
-            with open('valid_video.pkl', 'rb') as f:
-                self.valid_video = pickle.load(f)
+            self.valid_video = torch.load('valid_video.tar')
+
+            if self.pool:
+                self.valid_video = [torch.mean(frames, dim=0) for _, frames in self.valid_video.items()]
+                self.valid_video = torch.stack(self.valid_video)
 
             self.valid_label = getVideoList('HW5_data/TrimmedVideos/label/gt_valid.csv')['Action_labels']
             self.valid_label = np.array(self.valid_label).astype(np.uint8)
-            self.valid_label = torch.from_numpy(self.valid_label).type(torch.long)
+            self.valid_label = torch.LongTensor(self.valid_label)
 
     def __getitem__(self, index):
         video = self.train_video[index] if self.mode == 'train' else self.valid_video[index]
