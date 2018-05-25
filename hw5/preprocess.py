434d968e89f04fc635dbd7d1de291b7deb0ce30c
fanoping <fanoping@gmail.com>
2018-05-25 23:13:08 +0800

    preprocess fix

--- hw5/preprocess.py
+++ hw5/preprocess.py
@@ -1,6 +1,5 @@
 from utils.reader import getVideoList, readShortVideo
 import torch
-import pickle
 import argparse
 import sys
 
@@ -9,23 +8,18 @@ def main(args):
         preprocess of training/validation video frames
     """
     data_list = getVideoList(args.csv_file)
-    all_frames = []
+    all_frames = {}
     for i, (category, name) in enumerate(zip(data_list['Video_category'],
                                               data_list['Video_name'])):
         print("Loading: Category: {} Name: {}".format(category, name), end=' ')
-        frames = readShortVideo(args.video_dir, category, name, 6, 1)
+        frames = readShortVideo(args.video_dir, category, name, 12, 1)
         print(len(frames), 'frames', end='\r')
         sys.stdout.write('\033[K')
         frames = torch.stack(frames, 0)
-        # mean_pooling
-        all_frames.append(torch.mean(frames, dim=0))
+        all_frames[str(category+'/'+name)] = frames
 
-    all_frames = torch.stack(all_frames)
+    torch.save(all_frames, args.video_file)
     print('Frames of {} videos loaded.'.format(len(all_frames)))
-
-    with open(args.video_file, 'wb') as f:
-        pickle.dump(all_frames, f)
-
 
 
 if __name__ == '__main__':
@@ -34,7 +28,7 @@ if __name__ == '__main__':
                         help='action label csv file')
     parser.add_argument('--video-dir', default='HW5_data/TrimmedVideos/video/train',
                         help='video data for train/validation')
-    parser.add_argument('--video-file', default='train_video.pkl',
+    parser.add_argument('--video-file', default='train_video.tar',
                         help='video file for dumping pickle')
     parser.add_argument('--no-cuda', action='store_true',
                         help='use CPU in case there\'s no GPU support')
