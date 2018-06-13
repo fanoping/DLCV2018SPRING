b6bda0004565fc26f5435c319e499fe2c0049885
fanoping <fanoping@gmail.com>
2018-06-14 03:10:16 +0800

    finish structure

--- final/train.py
+++ final/train.py
@@ -1,16 +1,17 @@
-from trainer.supervised_trainer import SupervisedTrainer
+from trainer.mnist_trainer import MnistTrainer
+from trainer.fashion_mnist_trainer import FashionmnistTrainer
 import argparse
 
 
 def main(args):
-    trainer = eval(args.arch.title() + 'Trainer')(args)
+    trainer = eval(args.dataset.title() + 'Trainer')(args)
     trainer.train()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Final Project")
-    parser.add_argument('--arch', default='Supervised', type=str,
-                        help='training architecture [Supervised]')
+    parser.add_argument('--dataset', default='FashionMnist', type=str,
+                        help='training architecture [Mnist]')
     parser.add_argument('--train-dir', default='datasets/Fashion_MNIST_student/train',
                         help='training data directory')
     parser.add_argument('--test-dir', default='datasets/Fashion_MNIST_student/test',
@@ -21,8 +22,12 @@ if __name__ == '__main__':
                         help='training epochs (default: 100)')
     parser.add_argument('--log-step', default=1, type=int,
                         help='printing step size (default: 10')
+    parser.add_argument('--checkpoint', default='checkpoints/mnist/epoch50_checkpoint.pth.tar',
+                        help='load initialize weight from checkpoint')
     parser.add_argument('--no-cuda', action='store_true',
                         help='use CPU in case there\'s no GPU support')
+    parser.add_argument('--verbosity', action='store_true',
+                        help='validation visualization')
     parser.add_argument('--save-freq', default=1, type=int,
                         help='save checkpoints frequency (default: 1)')
     main(parser.parse_args())
--- final/utils/dataset.py
+++ final/utils/dataset.py
@@ -4,9 +4,9 @@ import numpy as np
 import os
 
 
-class Cifar10(Dataset):
+class FashionMNIST(Dataset):
     def __init__(self, args, mode='train', transform=None):
-        super(Cifar10, self).__init__()
+        super(FashionMNIST, self).__init__()
         self.args = args
         self.mode = mode
         self.transform = transform
@@ -20,14 +20,16 @@ class Cifar10(Dataset):
             base = self.args.train_dir
             classes_dir = sorted([os.path.join(base, file) for file in os.listdir(base) if not file.startswith('.DS')])
             for idx, dir in enumerate(classes_dir):
-                images = [np.expand_dims(imread(os.path.join(dir, image)), axis=2) for image in sorted(os.listdir(dir))]
+                images = [np.expand_dims(imread(os.path.join(dir, image)), axis=2)
+                          for image in sorted(os.listdir(dir)) if image.endswith(".png")]
                 labels = [idx for _ in range(len(images))]
                 self.image.extend(images)
                 self.label.extend(labels)
         else:
             base = self.args.test_dir
-            self.image = [np.expand_dims(imread(os.path.join(base, image)), axis=2) for image in sorted(os.listdir(base))]
-            self.label = [None for _ in range(len(self.image))]
+            self.image = [np.expand_dims(imread(os.path.join(base, image)), axis=2)
+                          for image in sorted(os.listdir(base)) if image.endswith(".png")]
+            self.label = [0 for _ in range(len(self.image))]
 
     def __getitem__(self, index):
         image = self.transform(self.image[index]) if self.transform is not None else self.image[index]
@@ -43,4 +45,4 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--train-dir', default='../datasets/Fashion_MNIST_student/train')
     parser.add_argument('--test-dir', default='../datasets/Fashion_MNIST_student/test')
-    data = Cifar10(parser.parse_args())
+    data = FashionMNIST(parser.parse_args())
